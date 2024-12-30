9d43db026bdba64fc4168938c290e22a90eb9a64
Ken Sedgwick <ken@bonsai.com>
2024-12-29 17:14:51 -0800

    improve logging format

--- noshtastic-app/src/main.rs
+++ noshtastic-app/src/main.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
+use chrono::Local;
 use clap::Parser;
+use env_logger::fmt::Formatter;
+use env_logger::Builder;
 use log::*;
+use std::env;
+use std::io::Write;
 use tokio::signal;
 use tokio::time::{sleep, Duration};
 
@@ -19,9 +24,31 @@ struct Args {
     serial: Option<String>,
 }
 
+fn init_logger() {
+    let mut builder = Builder::new();
+    if let Ok(rust_log) = env::var("RUST_LOG") {
+        builder.parse_filters(&rust_log);
+    } else {
+        builder.filter(None, log::LevelFilter::Debug); // Default to Debug
+    }
+    builder
+        .format(|buf: &mut Formatter, record: &Record| {
+            let now = Local::now();
+            writeln!(
+                buf,
+                "{} {:<5} {}: {}",
+                now.format("%H:%M:%S%.3f"),
+                record.level(),
+                record.target(),
+                record.args()
+            )
+        })
+        .init();
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    env_logger::init();
+    init_logger();
     let args = Args::parse();
     let (linkref, receiver) = create_link(&args.serial).await?;
     let syncref = Sync::new(linkref, receiver)?;
