39eaf1ecf6f87899443d4ba197bce4c1b3d367b7
Ken Sedgwick <ken@bonsai.com>
2025-01-02 22:15:54 -0800

    add copyright header to source files

--- noshtastic-link/build.rs
+++ noshtastic-link/build.rs
@@ -1,3 +1,8 @@
+// Copyright (C) 2025 Bonsai Software, Inc.
+// This file is part of Noshtastic, and is licensed under the
+// GNU General Public License, version 3 or later. See the LICENSE file
+// or <https://www.gnu.org/licenses/> for details.
+
 fn main() {
     tonic_build::configure()
         .build_server(false) // Don't build gRPC server if not needed
